33c5b6d7c152bad493adae208c62e9e0983ad689
Korppi <3602291+Korppi@users.noreply.github.com>
2022-10-06 20:54:28 +0300

    wrote some more tests

--- pkg/utils/utils_test.go
+++ pkg/utils/utils_test.go
@@ -33,3 +33,25 @@ func TestGetDescription(t *testing.T) {
 		t.Errorf("Got wrong description! Got \n%s\nwanted\n'There might be some problems but give it a try!'", desc)
 	}
 }
+
+func TestCountCharacters(t *testing.T) {
+	res, _ := countCharacters("AABBabc")
+	// TODO: better way to test slices?
+	if res[0] != 3 || res[1] != 3 || res[2] != 1 {
+		t.Errorf("Wrong count! got %v wanted {3,3,1}", res)
+	}
+}
+
+func TestStringToSet(t *testing.T) {
+	res := stringToSet("Aa")
+	if len(res) != 1 || res[0] != "a" {
+		t.Errorf("Wrong set! got %v wanted {a}", res)
+	}
+}
+
+func TestStringOccurencesToString(t *testing.T) {
+	res, _ := stringOccurencesToString("AaBbac")
+	if res != "321" {
+		t.Errorf("Wrong occurences counted! Got %s wanted '321'", res)
+	}
+}
